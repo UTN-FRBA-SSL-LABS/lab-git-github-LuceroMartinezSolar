8bbb400e0b5a02e35334cb9b9cd3fe655fba0f8d
LuceroMartinezSolar <lmartinezsolar@frba.utn.edu.ar>
2026-05-03 01:11:23 +0000

    Agrego comentario explicativo en multiplcar

--- operaciones.c
+++ operaciones.c
@@ -13,6 +13,8 @@ int restar(int a, int b) {
 }
 
 /* ── multiplicar — implementar en feature/mi-funcion ─────────────────────── */
+/* ──multiplica dos números enteros y devuelve el resultado─────────────────>
+
 
 int multiplicar(int a, int b) {
     return a * b; /* TODO: implementar */
