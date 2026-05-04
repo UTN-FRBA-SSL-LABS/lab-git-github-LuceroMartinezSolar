79baa947a2c6d1ca626b73a3e641ea77f34f8d42
ludigregorio@frba.utn.edu.ar <ludigregorio@frba.utn.edu.ar>
2026-05-03 22:00:09 -0300

    mejora : de la explicacion de multiplicacion

--- operaciones.c
+++ operaciones.c
@@ -2,7 +2,7 @@
 
 /* ── sumar — ya implementada, referencia de estilo ───────────────────────── */
 
-int sumar(int a, int b) {
+int sumar(const int a, const int b) {
     return a + b;
 }
 
@@ -13,7 +13,8 @@ int restar(int a, int b) {
 }
 
 /* ── multiplicar — implementar en feature/mi-funcion ─────────────────────── */
-/*Se aplica la multiplicación a dos números y se obtiene su resultado*/
+
+//esta funcion multiplica dos numeros y devuelve el resultado
 int multiplicar(int a, int b) {
     return a * b; /* TODO: implementar */
 }
